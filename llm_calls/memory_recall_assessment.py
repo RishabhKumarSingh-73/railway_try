757f48215b8b60a18efab00f3788a7723ddbd5a1
RishabhKumarSingh-73 <rishabhkumarsinghb@gmail.com>
2025-03-14 12:12:10 +0530

    memory_assessment_api_updated

---
+++ llm_calls/memory_recall_assessment.py
@@ -0,0 +1,51 @@
+from openai import OpenAI
+
+async def memory_assessment():
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key="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"
+    )
+
+    completion = client.chat.completions.create(
+        model="openai/gpt-3.5-turbo-0613",
+        messages=[
+            {
+                "role": "user",
+                "content": """Generate a paragraph of approximately 100 words that contains enough information to create six MCQs, such that these 6 MCQs will be asked from the generated paragraph.
+
+The paragraph should contain facts, events, and relationships that can be recalled directly or with slight inference.
+
+There should be 2 easy, 2 medium, and 2 hard multiple-choice questions,all of them shuffled with no sub topics as easy, hard, medium . 
+
+Easy: Direct recall of explicit facts from the paragraph.
+Medium: Requires slight inference or understanding of relationships. 
+Hard: Requires deeper memory and indirect recall.
+
+Important: The six MCQs must be randomly shuffled in the final output, with no order based on difficulty level.
+
+Each question should have 4 answer choices, one correct answer, and a difficulty level. the difficulty level should be numbers 1 for easy, 3 for medium, 5 for hard. and correct answer should have only the option a,b,c or d not the whole answer.
+
+the response should be in the following json format exactly.
+{{
+    "paragraph": "generated paragraph",
+    "questions": [ 
+        {{
+            "question": "Your question here",
+            "choices": [
+                ["A", "Your first choice here"],
+                ["B", "Your second choice here"],
+                ["C", "Your third choice here"],
+                ["D", "Your fourth choice here"]
+            ],
+            "correct_answer": "Correct choice letter (e.g., 'A')",
+            "difficulty": "Choose from: 1,3,5"
+        }}
+    ]
+}}
+
+"""
+            }
+        ]
+    )
+
+    return completion.choices[0].message.content
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import json
 import os
 from fastapi import FastAPI
 import uvicorn
+
+from llm_calls.memory_recall_assessment import memory_assessment
 
 app = FastAPI()
 
@@ -11,6 +14,23 @@ def home():
 @app.get("/greet/{name}")
 def greet(name: str):
     return {"message": f"Hello, {name}!"}
+
+#assessment
+@app.get("/memory")
+async def get_memory_recall_assessment_questions():
+    questions_str = await memory_assessment()
+    data = json.loads(questions_str)
+    return data
+
+#roadmap
+
+
+#mindmap
+
+
+#vedio
+
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))  # Default to 8000 if not set
